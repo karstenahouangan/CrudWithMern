d9c1e157cffa95e8d2401175c817c19b1c8cf495
Karsten .D <karstenahouangan@gmail.com>
2022-11-28 10:34:07 +0100

    configure index.js

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+const PORT = 8080
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}...`)
+})
