3029696965af715a54a69e8e2150b905a80f76ae
Karsten .D <karstenahouangan@gmail.com>
2022-12-01 14:44:36 +0100

    ok ça marche

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const express = require('express')
-const cors = require('cors')
 const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const todoRoute = require('./routes/todos')
+const mongoose = require('mongoose')
 
-app.use(express.json())
+const DataAN = "todoDB";
+const dbURL = `mongodb://localhost:27017/${DataAN}`;
+
 app.use(cors())
-const PORT = 8080
-app.listen(PORT, () => {
-    console.log(`Server is running on PORT ${PORT}...`)
+app.use(bodyParser.json())
+app.use('/todos', todoRoute)
+mongoose.connect(dbURL).then(data => {
+    console.log("connected to DB")
+}).catch(error => {
+    console.log(error)
 })
+app.listen(3000)
